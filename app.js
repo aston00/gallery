b6dfa1b7617b0f4493614c7ebd949bb640b72124
aston00 <jacksterwow@gmail.com>
2018-04-29 20:46:11 +0300

    made some styling adjustments

--- app.js
+++ app.js
@@ -1,25 +1,25 @@
 angular.module('app', ['ui.router']);
 
 
+angular.module('app').config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider){
+
+    $urlRouterProvider.otherwise('/');
+
+    $stateProvider
+        .state({
+            name: 'home',
+            url: '/',
+            template: '<app-showcase></app-showcase>'
+        })
+        
+}])
+
+
 // require('./directives/clipboard.directive');
 require('./components/app-nav-bar/appNavBar.component');
 require('./components/showcase/showcase.component');
 require('./main.css');
 // require('./components/skillsPage/skills.component');
-
-// angular.module('app').config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider){
-
-//     $urlRouterProvider.otherwise('/');
-
-//     $stateProvider
-//         .state({
-//             name: 'home',
-//             url: '/',
-//             template: '<app-showcase></app-showcase>'
-//         })
-        
-// }])
-
 
 
 
